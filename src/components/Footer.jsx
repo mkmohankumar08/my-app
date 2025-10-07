eb3ba6de739991a368217e8bb023fb8733164250
mkmohankumar08 <mkmohankumar0812@gmail.com>
2025-10-07 22:44:00 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaWhatsapp } from 'react-icons/fa';
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaWhatsapp } from 'react-icons/fa' ;
 
 const Footer = () => {
   return (
